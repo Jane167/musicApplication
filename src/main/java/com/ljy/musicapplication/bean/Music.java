9aae081d40f501a7a5daa74769f0708680506eab
jiayin-wait <2480243521@qq.com>
2022-06-23 11:26:04 +0800

    feature: 添加Music实体类

---
+++ src/main/java/com/ljy/musicapplication/bean/Music.java
@@ -0,0 +1,113 @@
+package com.ljy.musicapplication.bean;
+
+/**
+ * music表对应的实体类
+ */
+public class Music {
+    private int musicId;           // 编号
+    private String musicName;      // 音乐名称
+    private String musicPic;       // 封面图片
+    private String singer;         // 歌手
+    private int musictypeId;       // 类别编号
+    private String recordCompany;  // 唱片公司
+    private String releaseDate;    // 发行时间
+    private String depict;         // 描述
+    private double price;          // 价格
+    private int status;            // 状态
+
+    public int getMusicId() {
+        return musicId;
+    }
+
+    public void setMusicId(int musicId) {
+        this.musicId = musicId;
+    }
+
+    public String getMusicName() {
+        return musicName;
+    }
+
+    public void setMusicName(String musicName) {
+        this.musicName = musicName;
+    }
+
+    public String getMusicPic() {
+        return musicPic;
+    }
+
+    public void setMusicPic(String musicPic) {
+        this.musicPic = musicPic;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    public int getMusictypeId() {
+        return musictypeId;
+    }
+
+    public void setMusictypeId(int musictypeId) {
+        this.musictypeId = musictypeId;
+    }
+
+    public String getRecordCompany() {
+        return recordCompany;
+    }
+
+    public void setRecordCompany(String recordCompany) {
+        this.recordCompany = recordCompany;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getDepict() {
+        return depict;
+    }
+
+    public void setDepict(String depict) {
+        this.depict = depict;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Music{" +
+                "musicId=" + musicId +
+                ", musicName='" + musicName + '\'' +
+                ", musicPic='" + musicPic + '\'' +
+                ", singer='" + singer + '\'' +
+                ", musictypeId=" + musictypeId +
+                ", recordCompany='" + recordCompany + '\'' +
+                ", releaseDate='" + releaseDate + '\'' +
+                ", depict='" + depict + '\'' +
+                ", price=" + price +
+                ", status=" + status +
+                '}';
+    }
+}
